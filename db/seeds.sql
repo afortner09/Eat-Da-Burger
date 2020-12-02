ab1892229e58a75f4531ec2f6be9b3e5f19b050a
Alejandro Fortner <alejandroc0912@gmail.com>
2020-12-01 20:12:09 -0500

    added files to new repo

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db
+
+INSERT INTO burgers (burger_name) VALUES ('Sweet Bacon Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('The Weight Loss Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Shopper', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Marben', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Stockyard burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Golden Plate');
